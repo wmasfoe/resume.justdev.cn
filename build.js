253ad991bac083d583b286d2b24cc51201f5dee0
lijiaqi <lijiaqi@xiaohongshu.net>
2023-04-02 17:41:07 +0800

    fix: puppeteer

--- build.js
+++ build.js
@@ -26,7 +26,7 @@ async function buildHTML() {
 }
 
 async function buildPDF(html) {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage();
   console.log('Opening puppeteer...')
   await page.setContent(html, { waitUntil: 'networkidle0' })
