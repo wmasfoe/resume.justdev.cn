68b92552122227c5a8461d4f7d8e18d40c625932
wmasfoe <lijq1103@163.com>
2025-03-24 00:49:02 +0800

    feat: 简历内容

--- app/components/TargeVersion/index.tsx
+++ app/components/TargeVersion/index.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import Link from 'next/link';
-import styles from './style.module.css';
+'use client'
+import React, { useMemo } from 'react'
+import Link from 'next/link'
+import styles from './style.module.css'
+import useBreakpoints, { MediaType } from '@/hooks/use-breakpoints'
 
 /**
  * 版本切换组件，用于区分PDF和在线版本
  */
 const TargetVersion: React.FC = () => {
+  const media = useBreakpoints()
+  const isMobile = useMemo(() => [MediaType.tablet, MediaType.mobile].includes(media), [media])
+
   return (
     <div className={styles.versionContainer}>
       {/* 只在在线版本显示 */}
       <div className={styles.onlineOnly}>
         <p className={styles.versionText}>
           <Link
-            href="/pdfs/resume.pdf" 
+            href="/pdfs/resume.pdf"
             target="_blank"
             className={styles.versionLink}
           >
-            前往 PDF 版本
+            {
+              isMobile
+                ? 'PDF 版本'
+                : '前往 PDF 版本'
+            }
             <svg width="14" height="14" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M19 11H37V29" stroke="var(--foreground)" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
               <path d="M11.5439 36.4559L36.9997 11" stroke="var(--foreground)" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
@@ -28,8 +37,8 @@ const TargetVersion: React.FC = () => {
       {/* 只在PDF版本显示 */}
       <div className={styles.pdfOnly}>
         <p className={styles.versionText}>
-          <Link 
-            href="https://resume.xhub.xin" 
+          <Link
+            href="https://resume.xhub.xin"
             target="_blank"
             className={styles.versionLink}
           >
@@ -42,9 +51,9 @@ const TargetVersion: React.FC = () => {
         </p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-TargetVersion.displayName = 'TargetVersion';
+TargetVersion.displayName = 'TargetVersion'
 
-export default TargetVersion;
+export default TargetVersion
